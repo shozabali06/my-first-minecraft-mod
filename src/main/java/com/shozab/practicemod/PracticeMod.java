7f6ceadc2031af41c655591e9a09140a16b12591
Shozab <shozabali06@gmail.com>
2026-02-08 09:15:30 +0500

    Add custom items: pink_garnet, raw_pink_garnet

--- src/main/java/com/shozab/practicemod/PracticeMod.java
+++ src/main/java/com/shozab/practicemod/PracticeMod.java
@@ -1,5 +1,6 @@
 package com.shozab.practicemod;
 
+import com.shozab.practicemod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -18,6 +19,8 @@ public class PracticeMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
---
+++ src/main/java/com/shozab/practicemod/item/ModItems.java
@@ -0,0 +1,28 @@
+package com.shozab.practicemod.item;
+
+import com.shozab.practicemod.PracticeMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
+    public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(PracticeMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        PracticeMod.LOGGER.info("Registering Mod Items for " + PracticeMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(PINK_GARNET);
+            entries.add(RAW_PINK_GARNET);
+        });
+    }
+}

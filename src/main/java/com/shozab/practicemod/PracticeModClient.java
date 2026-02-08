6014d9eddc58500ea8914a15bf5d4082348af7d1
Shozab <shozabali06@gmail.com>
2026-02-08 08:35:00 +0500

    Initial commit

---
+++ src/main/java/com/shozab/practicemod/PracticeModClient.java
@@ -0,0 +1,10 @@
+package com.shozab.practicemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PracticeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
